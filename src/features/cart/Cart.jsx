5c01f2da3cb535fa01256f6076669ffb88653bc9
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-07-25 16:26:30 +0330

    show and hide cart in footer

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -29,21 +29,22 @@ function Cart() {
 
   return (
     <>
-      <button
-        type="button"
-        className={`btn btn-info btn-lg pay-off ${
-          themeValue === "light"
-            ? "pay-off-Backgroundlight"
-            : "pay-off-BackgroundDark"
-        }`}
-        // data-toggle="modal"
-        // data-target="#myModal"
-        onClick={openPopupCart}
-      >
-        <span>{t(generaltext.bill)}</span>
-        <span className="bill"> {totalprice} </span>
-        <span>{t(generaltext.MonetaryUnit)}</span>
-      </button>
+      {totalprice !== 0 && (
+        <button
+          type="button"
+          className={`btn btn-info btn-lg pay-off ${
+            themeValue === "light"
+              ? "pay-off-Backgroundlight"
+              : "pay-off-BackgroundDark"
+          }`}
+          onClick={openPopupCart}
+        >
+          <span>{t(generaltext.bill)}</span>
+          <span className="bill"> {totalprice} </span>
+          <span>{t(generaltext.MonetaryUnit)}</span>
+        </button>
+      )}
+
       <CartPopup
         showCartPopup={showCartPopup}
         setShowCartPopup={setShowCartPopup}
